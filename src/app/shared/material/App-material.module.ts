59420501d69faa2fd1f91c02a792810882adfa01
Fernandolilo <fernando.sistenpro@outlook.com>
2022-10-13 07:40:18 -0300

    HTML a login

--- src/app/shared/material/App-material.module.ts
+++ src/app/shared/material/App-material.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 
 
@@ -9,7 +11,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   exports: [
     MatTableModule,
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule
   ],
 })
 export class AppMaterialModule { }
